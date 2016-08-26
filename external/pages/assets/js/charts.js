b51663f416d256cf838c8046fca8c9c890a69a76
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2016-08-26 17:01:30 +0100

    add click data point to compare commits on github to benchmark charts

--- external/pages/assets/js/charts.js
+++ external/pages/assets/js/charts.js
@@ -1,5 +1,5 @@
 var chartingLib = {
-      createOpts: function(title, labelsDiv, yLabel) {
+      createOpts: function(title, labelsDiv, yLabel, commits) {
         return { "title": title,
             "colors": ["#00BFB3", "#FED10A", "#0078A0", "#DF4998", "#93C90E", "#00A9E5", "#222", "#AAA", "#777"],
             "axisLabelColor": "#555",
@@ -15,24 +15,51 @@ var chartingLib = {
             "legend": "always",
             "drawPoints": true,
             "pointSize": 3,
-            "gridLineColor": "#BBB"
+            "gridLineColor": "#BBB",
+            pointClickCallback: function(e, p) {
+              var commitInfo = commits[p.xval];
+              if (commitInfo) {
+                var url = "https://github.com/elastic/elasticsearch/compare/" + commitInfo.prev_commit + "..." +  commitInfo.this_commit;
+                window.open(url);
+              }
+            }
             //"strokePattern": [5, 10]
         }
     },
 
       renderChart: function(elementId, title, yLabel, annotationSource) {
-          var g = new Dygraph(
-              document.getElementById("chart_" + elementId),
-              elementId + ".csv",
-              this.createOpts("<a href='#" + elementId + "'>" + title + "</a>", "chart_" + elementId + "_labels", yLabel)
-          );
-          g.ready(function() {
-            var annotationSource = elementId + '_annotations.json'
-            $.getJSON(annotationSource, function(json) {
-              g.setAnnotations(json);
-            });
+        var commits = {};
+        var g = new Dygraph(
+            document.getElementById("chart_" + elementId),
+            elementId + ".csv",
+            this.createOpts("<a href='#" + elementId + "'>" + title + "</a>", "chart_" + elementId + "_labels", yLabel, commits)
+        );
+        g.ready(function() {
+          var annotationSource = elementId + '_annotations.json'
+          $.getJSON(annotationSource, function(json) {
+            g.setAnnotations(json);
           });
-          return g;
+          $.ajax({
+            type: 'GET',
+            url: 'source_revision.csv',
+            dataType: 'text',
+            success: function(allText) {
+              var allTextLines = allText.split(/\r\n|\n/);
+
+              var previousCommit = null;
+              for (var i=1; i<allTextLines.length; i++) {
+                  var data = allTextLines[i].split(',');
+                  var date = new Date(data[0]);
+                  commits[date.getTime()] = {
+                    this_commit: data[1],
+                    prev_commit: previousCommit
+                  };
+                  previousCommit = data[1];
+              }
+            }
+          });
+        });
+        return g;
       },
 
       synchronize: function(graphs) {
