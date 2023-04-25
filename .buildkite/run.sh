0b904c0828b457e70f835008c58d4fd52b99609a
Quentin Pradet <quentin.pradet@elastic.co>
2023-04-25 10:58:40 +0400

    Fix effective start date support (#733)

--- .buildkite/run.sh
+++ .buildkite/run.sh
@@ -8,4 +8,4 @@ cd $HOME
 source ~/.profile
 export PATH=$HOME/bin:$HOME/.local/bin:$PATH
 
-night_rally --self-update --mode="$MODE" --version="$VERSION" --runtime-jdk="$RUNTIME_JDK" --release-license="$RELEASE_LICENSE" --target-host="$TARGET_HOSTS" --race-configs="$RACE_CONFIGS"
+night_rally --self-update --mode="$MODE" --version="$VERSION" --runtime-jdk="$RUNTIME_JDK" --release-license="$RELEASE_LICENSE" --target-host="$TARGET_HOSTS" --race-configs="$RACE_CONFIGS" --effective-start-date="$EFFECTIVE_START_DATE"
